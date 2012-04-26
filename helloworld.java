fa172fde8e41155a168c1a08dcd638ce72badbef
huma <huma@MyLaptop.(none)>
2012-04-26 12:47:45 +0200

    Just for testing

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
